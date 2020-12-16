13751e25c83191c460d1b22f8d2c3a99b36a1ef4
JaeHongMin94 <moonshin123@naver.com>
2020-12-17 05:12:44 +0900

    반복문

--- Example/JS_Example/iteration.js
+++ Example/JS_Example/iteration.js
@@ -4,7 +4,7 @@
     반복이 되는 코드 블럭
   }
 */
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   console.log('안녕하세요.' + i);
 }
 
@@ -14,7 +14,7 @@ console.log();
 console.log('for(;;)');
 for (;;) {
   console.log('안녕하세요');
-  if (Math.random() * 100 > 90) break; // 계속 반복하게 되면 컴퓨터에 무리가 가기때문에 랜덤하게 멈춰준다.
+  if (Math.random() * 10 > 5) break; // 계속 반복하게 되면 컴퓨터에 무리가 가기때문에 랜덤하게 멈춰준다.
 }
 
 console.log();
@@ -28,7 +28,7 @@ console.log();
 console.log('while()');
 while (true) {
   console.log('안녕하세요');
-  if (Math.random() * 100 > 90) break;
+  if (Math.random() * 10 > 5) break;
 }
 
 console.log();
@@ -59,10 +59,58 @@ console.log();
 
 /*
   for in
+  객체와 같이 사용
 */
-var obj = { a: 1, b: 2, c: 3 };
-
+var doggy = {
+  name: '멍멍이',
+  sound: '멍멍',
+  age: 2,
+};
+console.log(Object.entries(doggy));
+console.log(Object.keys(doggy));
+console.log(Object.values(doggy));
 console.log('for in');
-for (const prop in obj) {
-  console.log(`obj.${prop} = ${obj[prop]}`);
+for (const key in doggy) {
+  console.log(`${key}: ${doggy[key]}`);
 }
+
+console.log();
+
+// continue, break
+for (let i = 0; i < 10; i++) {
+  if (i === 2) continue; // 반복문으로 돌아간다.
+  console.log(i);
+  if (i === 5) break; // 반복문을 종료한다.
+}
+
+console.log();
+
+// 숫자로 이루어진 배열이 주어졌을 때, 해당 숫자 배열안에 들어있는 숫자 중 3보다 큰 숫자로만 이루어진 배열을 새로 만들어서 반환해보세요.
+function biggerThanThree(numbers) {
+  // 내가 짠 코드
+  /* 구현해보세요 */
+  let array = [];
+  for (let i = 0, j = 0; i < numbers.length; i++) {
+    if (numbers[i] > 3) {
+      array[j] = numbers[i];
+      j++;
+    }
+  }
+  return array;
+}
+
+function biggerThanThree2(numbers) {
+  // 출제자 정답 코드
+  k = [];
+  for (let number of numbers) {
+    if (number > 3) {
+      k.push(number);
+    }
+  }
+
+  return k;
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(numbers)); // [4, 5, 6, 7]
+console.log(biggerThanThree2(numbers)); // [4, 5, 6, 7]
