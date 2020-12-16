9388bb5a443bb4ea55d3eacd367d1d76d343cf51
JaeHongMin94 <moonshin123@naver.com>
2020-12-16 17:53:50 +0900

    클래스

--- Example/JS_Example/class.js
+++ Example/JS_Example/class.js
@@ -248,3 +248,49 @@ class Parent4 {
 
 class Child4 extends Parent4 {}
 console.log(Parent4.age, Child4.age);
+
+// get, set study
+const dog = {
+  _name: '멍멍이',
+  get name() {
+    console.log('_name을 조회합니다...');
+    return this._name;
+  },
+  set name(value) {
+    console.log('이름이 바뀝니다..' + value);
+    this._name = value;
+  },
+};
+
+dog.name = '뭉뭉이';
+console.log(dog.name);
+
+console.log();
+
+const numbers = {
+  _a: 1,
+  _b: 2,
+  sum: 3,
+  calculate() {
+    console.log('calculate');
+    this.sum = this._a + this._b;
+  },
+  get a() {
+    return this._a;
+  },
+  get b() {
+    return this._b;
+  },
+  set a(value) {
+    this._a = value;
+    this.calculate();
+  },
+  set b(value) {
+    this._b = value;
+    this.calculate();
+  },
+};
+
+console.log(numbers.sum);
+numbers.a = 5;
+console.log(numbers.sum);
