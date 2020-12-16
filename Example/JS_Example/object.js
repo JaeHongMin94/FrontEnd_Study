619ee4bd0b2220e52c3a68a29de52a724875a89f
JaeHongMin94 <moonshin123@naver.com>
2020-12-16 17:53:54 +0900

    객체

--- Example/JS_Example/object.js
+++ Example/JS_Example/object.js
@@ -156,3 +156,46 @@ console.log(area2 instanceof Object);
 
 console.log(area2.slice(0, 2));
 console.log(Array.prototype.slice, Object.prototype.slice);
+
+console.log();
+
+// 객체 비구조화 할당
+const ironMan = {
+  name: '토니스타크',
+  actor: '로버트 다우니 주니어',
+  alias: '아이언맨',
+};
+
+const captainAmerica = {
+  name: '스티븐 로저스',
+  actor: '크리스 에반스',
+  alias: '캡틴 아메리카',
+};
+
+function print({ alias, name, actor }) {
+  const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
+  console.log(text);
+}
+
+print(ironMan);
+print(captainAmerica);
+
+console.log();
+
+// 객체 안에 함수 넣기
+const dog = {
+  name: '멍멍이',
+  sound: '멍멍!',
+  say: function () {
+    console.log(this.sound);
+  },
+};
+
+const cat = {
+  name: '야옹이',
+  sound: '야옹~',
+};
+
+cat.say = dog.say;
+dog.say();
+cat.say();
