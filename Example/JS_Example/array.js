4ea63cffd223a85889b9c0d3a891f9c6747dc4f9
JaeHongMin94 <moonshin123@naver.com>
2020-12-17 05:12:37 +0900

    배열, 배열 내장함수

--- Example/JS_Example/array.js
+++ Example/JS_Example/array.js
@@ -27,3 +27,112 @@ console.log(array3[2], typeof array3[2]);
 console.log(array3[3], typeof array3[3]);
 console.log(array3[4], typeof array3[4]);
 console.log(array3[5], typeof array3[5]);
+
+console.log();
+
+// 배열 내장함수
+const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
+
+// 방법1
+function print(hero) {
+  console.log(hero);
+}
+superheroes.forEach(print);
+
+console.log('-');
+
+// 방법2
+superheroes.forEach(function (hero) {
+  console.log(hero);
+});
+
+console.log('-');
+
+// 방법3
+superheroes.forEach((hero) => {
+  console.log(hero);
+});
+
+console.log();
+
+// map
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squared = [];
+numbers.forEach((n) => {
+  squared.push(n * n);
+});
+
+console.log(squared);
+
+const square = numbers.map((n) => n * n);
+console.log(square);
+
+console.log();
+
+const items = [
+  {
+    id: 1,
+    text: 'hello',
+  },
+  {
+    id: 2,
+    text: 'bye',
+  },
+];
+const texts = items.map((item) => item.text);
+console.log(texts);
+
+console.log();
+
+// indexOf
+const index = superheroes.indexOf('토르');
+console.log(index);
+
+// findIndex, find
+const todos = [
+  {
+    id: 1,
+    text: '자바스크립트 입문',
+    done: true,
+  },
+  {
+    id: 2,
+    text: '함수 배우기',
+    done: true,
+  },
+  {
+    id: 3,
+    text: '객체와 배열 배우기',
+    done: true,
+  },
+  {
+    id: 4,
+    text: '배열 내장함수 배우기',
+    done: false,
+  },
+];
+
+const index2 = todos.find((todo) => todo.done === false);
+console.log(index2);
+
+// filter
+const tasksNotDone = todos.filter((todo) => todo.done);
+console.log(tasksNotDone);
+
+/*
+  splice, slice
+  splice는 기존의 배열을 건드리고, slice는 기존의 배열을 건드리지 않는다.
+*/
+// numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const index3 = numbers.indexOf(4);
+const spliced = numbers.splice(index3, 3);
+console.log(spliced);
+console.log(numbers);
+
+console.log();
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8];
+const sliced = numbers.slice(0, 2);
+console.log(sliced);
+console.log(numbers2);
