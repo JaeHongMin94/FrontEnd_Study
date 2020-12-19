0515c26f227961c86b2f19e0ec3bb2d21f83ea68
JaeHongMin94 <moonshin123@naver.com>
2020-12-19 12:29:59 +0900

    조건문

--- Example/JS_Example/conditional.js
+++ Example/JS_Example/conditional.js
@@ -113,3 +113,12 @@ switch (n % 5) {
   default:
     console.log(n);
 }
+
+/*
+  삼항 연산자
+  condition ? true : false
+*/
+const array = [];
+let text =
+  array.length === 0 ? '배열이 비어있습니다.' : '배열이 비어있지 않습니다.';
+console.log(text);
